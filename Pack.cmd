a985c36f782fcd86aa46902a7c2f15be65353af9
Sam Lu <549429518@qq.com>
2023-11-15 20:29:29 +0800

    Update Pack.cmd

--- Pack.cmd
+++ Pack.cmd
@@ -1,1 +1,6 @@
-.\Build.cmd -Configuration Release -pack
+@echo off
+if not "%1"=="" (
+    set args=/p:PackageVersion=%1 /p:SemanticVersioningV1=false
+)
+@echo on
+.\Build.cmd -Configuration Release -pack %args%
